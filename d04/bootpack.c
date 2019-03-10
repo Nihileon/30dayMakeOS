6191018c129cfdc9ff33bd24a28895d84d0af4aa
ningbaixuan@163.com <6077521+Animam@users.noreply.github.com>
2019-03-10 14:50:33 +0800

    day6.1

--- d04/bootpack.c
+++ d04/bootpack.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
+#include "bootpack.h"
 
-// 利用宏来枚举
 #define COL8_000000 0
 #define COL8_FF0000 1
 #define COL8_00FF00 2
@@ -18,32 +18,13 @@
 #define COL8_008484 14
 #define COL8_848484 15
 
-void io_hlt(void);
-void io_cli(void);
-void io_out8(int port, int data);
-int io_load_eflags(void);
-void io_store_eflags(int eflags);
 
-void init_palette(void);
-void set_palette(int start, int end, unsigned char *rgb);
-void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0,
-              int x1, int y1);
-void int_screen8(char *varm, int x, int y);
-void putfont8(char *varm, int xsize, int x, int y, char c, char *font);
-void putfonts8_asc(char *vram, int xsize, int x, int y, char c,
-                   unsigned char *s);
-void init_mouse_cursor8(char *mouse, char bc);
-void putblock8_8(char *vram, int vcsize, int pxsize, int pysize, int px0,
-                 int py0, char *buf, int bxsize);
-
-/* 是函数声明却不用{}，而用;，这表示的意思是：
-        函数在别的文件中，你自己找一下 */
-
-struct BOOTINFO {
-    char cyls, leds, vmode, reserve;
-    short scrnx, scrny;
-    char *vram;
-};
+void init_gdtidt(void);
+void set_segmdesc(struct SEGMENT_DESCRIPTOR *sd, unsigned int limit, int base,
+                  int ar);
+void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar);
+void load_gdtr(int limit, int addr);
+void load_idtr(int limit, int addr);
 
 void HariMain(void)
 {
@@ -51,10 +32,10 @@ void HariMain(void)
     char s[40], mcursor[256];
     int mx, my;
 
+    init_gdtidt();
     init_palette();
-    int_screen8(binfo->vram, binfo->scrnx, binfo->scrny);
-
-    mx = (binfo->scrnx - 16) / 2;
+    init_screen8(binfo->vram, binfo->scrnx, binfo->scrny);
+    mx = (binfo->scrnx - 16) / 2; /* 画面中央になるように座標計算 */
     my = (binfo->scrny - 28 - 16) / 2;
     init_mouse_cursor8(mcursor, COL8_008484);
     putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
@@ -64,175 +45,4 @@ void HariMain(void)
     for (;;) {
         io_hlt();
     }
-}
-
-void init_palette(void)
-{
-    static unsigned char table_rgb[16 * 3] = {
-        0x00, 0x00, 0x00, /* 0: 黑*/
-        0xff, 0x00, 0x00, /* 1:亮红 */
-        0x00, 0xff, 0x00, /* 2:亮绿 */
-        0xff, 0xff, 0x00, /* 3:亮黄 */
-        0x00, 0x00, 0xff, /* 4:亮蓝 */
-        0xff, 0x00, 0xff, /* 5:亮紫 */
-        0x00, 0xff, 0xff, /* 6:浅亮蓝 */
-        0xff, 0xff, 0xff, /* 7:白 */
-        0xc6, 0xc6, 0xc6, /* 8:亮灰 */
-        0x84, 0x00, 0x00, /* 9:暗红 */
-        0x00, 0x84, 0x00, /* 10:暗绿 */
-        0x84, 0x84, 0x00, /* 11:暗黄 */
-        0x00, 0x00, 0x84, /* 12:暗青 */
-        0x84, 0x00, 0x84, /* 13:暗紫 */
-        0x00, 0x84, 0x84, /* 14:浅暗蓝 */
-        0x84, 0x84, 0x84  /* 15:暗灰 */
-    };
-    set_palette(0, 15, table_rgb);
-    return;
-}
-
-void set_palette(int start, int end, unsigned char *rgb)
-{
-    int i, eflags;
-    eflags = io_load_eflags();
-    for (i = start; i < end; i++) {
-        io_cli();
-        io_out8(0x03c9, rgb[0] / 4);
-        io_out8(0x03c9, rgb[1] / 4);
-        io_out8(0x03c9, rgb[2] / 4);
-        rgb += 3;
-    }
-    io_store_eflags(eflags);
-    return;
-}
-
-void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0,
-              int x1, int y1)
-{
-    int x, y;
-    for (y = y0; y <= y1; y++) {
-        for (x = x0; x <= x1; x++) {
-            vram[y * xsize + x] = c;
-        }
-    }
-    return;
-}
-
-void int_screen8(char *vram, int x, int y)
-{
-
-    boxfill8(vram, x, COL8_008484, 0, 0, x - 1, y - 29);
-    boxfill8(vram, x, COL8_C6C6C6, 0, y - 28, x - 1, y - 28);
-    boxfill8(vram, x, COL8_FFFFFF, 0, y - 27, x - 1, y - 27);
-    boxfill8(vram, x, COL8_C6C6C6, 0, y - 26, x - 1, y - 1);
-
-    boxfill8(vram, x, COL8_FFFFFF, 3, y - 24, 59, y - 24);
-    boxfill8(vram, x, COL8_FFFFFF, 2, y - 24, 2, y - 4);
-    boxfill8(vram, x, COL8_848484, 3, y - 4, 59, y - 4);
-    boxfill8(vram, x, COL8_848484, 59, y - 23, 59, y - 5);
-    boxfill8(vram, x, COL8_000000, 2, y - 3, 59, y - 3);
-    boxfill8(vram, x, COL8_000000, 60, y - 24, 60, y - 3);
-
-    boxfill8(vram, x, COL8_848484, x - 47, y - 24, x - 4, y - 24);
-    boxfill8(vram, x, COL8_848484, x - 47, y - 23, x - 47, y - 4);
-    boxfill8(vram, x, COL8_FFFFFF, x - 47, y - 3, x - 4, y - 3);
-    boxfill8(vram, x, COL8_FFFFFF, x - 3, y - 24, x - 3, y - 3);
-    return;
-}
-
-void putfont8(char *vram, int xsize, int x, int y, char c, char *font)
-{
-    int i;
-    char *p, d /* data */;
-    for (i = 0; i < 16; i++) {
-        p = vram + (y + i) * xsize + x;
-        d = font[i];
-        if ((d & 0x80) != 0) {
-            p[0] = c;
-        }
-        if ((d & 0x40) != 0) {
-            p[1] = c;
-        }
-        if ((d & 0x20) != 0) {
-            p[2] = c;
-        }
-        if ((d & 0x10) != 0) {
-            p[3] = c;
-        }
-        if ((d & 0x08) != 0) {
-            p[4] = c;
-        }
-        if ((d & 0x04) != 0) {
-            p[5] = c;
-        }
-        if ((d & 0x02) != 0) {
-            p[6] = c;
-        }
-        if ((d & 0x01) != 0) {
-            p[7] = c;
-        }
-    }
-    return;
-}
-
-void putfonts8_asc(char *vram, int xsize, int x, int y, char c,
-                   unsigned char *s)
-{
-    extern char hankaku[4096];
-    for (; *s != 0x00; s++) {
-        putfont8(vram, xsize, x, y, c, hankaku + *s * 16);
-        x += 8;
-    }
-    return;
-}
-
-// mouse: 输入的鼠标图形
-// bc: 背景颜色
-void init_mouse_cursor8(char *mouse, char bc)
-{
-    static char cursor[16][16] = {
-        "**************..", "*OOOOOOOOOOO*...", "*OOOOOOOOOO*....",
-        "*OOOOOOOOO*.....", "*OOOOOOOO*......", "*OOOOOOO*.......",
-        "*OOOOOOO*.......", "*OOOOOOOO*......", "*OOOO**OOO*.....",
-        "*OOO*..*OOO*....", "*OO*....*OOO*...", "*O*......*OOO*..",
-        "**........*OOO*.", "*..........*OOO*", "............*OO*",
-        ".............***"};
-    int x, y;
-
-    for (y = 0; y < 16; y++) {
-        for (x = 0; x < 16; x++) {
-            switch (cursor[x][y]) {
-            case '*':
-                mouse[y * 16 + x] = COL8_000000;
-                break;
-            case 'O':
-                mouse[y * 16 + x] = COL8_FFFFFF;
-                break;
-            case '.':
-                mouse[y * 16 + x] = bc;
-                break;
-            default:
-                break;
-            }
-        }
-    }
-    return;
-}
-// vram == 0xa0000
-// vxsize == 320 整个显示屏的宽度
-// pxsize : 需要输出的x大小
-// pysize: 需要输出的y大小
-// px0: 输出位置
-// py0: 输出位置
-// buf: 鼠标地址
-// bxsize: 鼠标大小
-void putblock8_8(char *vram, int vxsize, int pxsize, int pysize, int px0,
-                 int py0, char *buf, int bxsize)
-{
-    int x, y;
-    for (y = 0; y < pysize; y++) {
-        for (x = 0; x < pxsize; x++) {
-            vram[(py0 + y) * vxsize + (px0 + x)] = buf[y * bxsize + x];
-        }
-    }
-    return;
 }
