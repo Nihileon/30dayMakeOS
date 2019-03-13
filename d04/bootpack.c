7c35474d5b0dcc77169698a9faf8469e383c562d
ningbaixuan@163.com <6077521+Animam@users.noreply.github.com>
2019-03-13 14:53:25 +0800

    day7 e

--- d04/bootpack.c
+++ d04/bootpack.c
@@ -1,38 +1,28 @@
-#include <stdio.h>
+/*
+ * @Author: Nihil Eon
+ * @Date: 2019-03-13 10:37:45
+ * @Last Modified by: Nihil Eon
+ * @Last Modified time: 2019-03-13 14:50:45
+ */
 #include "bootpack.h"
+#include <stdio.h>
 
-#define COL8_000000 0
-#define COL8_FF0000 1
-#define COL8_00FF00 2
-#define COL8_FFFF00 3
-#define COL8_0000FF 4
-#define COL8_FF00FF 5
-#define COL8_00FFFF 6
-#define COL8_FFFFFF 7
-#define COL8_C6C6C6 8
-#define COL8_840000 9
-#define COL8_008400 10
-#define COL8_848400 11
-#define COL8_000084 12
-#define COL8_840084 13
-#define COL8_008484 14
-#define COL8_848484 15
-
-
-void init_gdtidt(void);
-void set_segmdesc(struct SEGMENT_DESCRIPTOR *sd, unsigned int limit, int base,
-                  int ar);
-void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar);
-void load_gdtr(int limit, int addr);
-void load_idtr(int limit, int addr);
+extern struct FIFO8 keyinfo;
 
 void HariMain(void)
 {
-    struct BOOTINFO *binfo = (struct BOOTINFO *)0x0ff0;
-    char s[40], mcursor[256];
+    struct BOOTINFO *binfo = (struct BOOTINFO *)ADR_BOOTINFO;
+    char s[40], mcursor[256], keybuf[32];
     int mx, my;
-
+    int i;
     init_gdtidt();
+    init_pic();
+    io_sti(); // sti是cli的逆指令, 执行sti后, if(中断许可标志)变为1,
+              // cpu可以接受外部中断
+    fifo8_init(&keyinfo, 32, keybuf);
+    io_out8(PIC0_IMR, 0xf9); //中断许可11111001 , 开放PIC1和键盘中断
+    io_out8(PIC1_IMR, 0xef); // 开放鼠标中断(11101111)
+
     init_palette();
     init_screen8(binfo->vram, binfo->scrnx, binfo->scrny);
     mx = (binfo->scrnx - 16) / 2; /* 画面中央になるように座標計算 */
@@ -43,6 +33,15 @@ void HariMain(void)
     putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
 
     for (;;) {
-        io_hlt();
+        io_cli();
+        if(fifo8_status(&keyinfo) == 0){
+            io_stihlt();
+        }else{
+            i = fifo8_get(&keyinfo);
+            io_sti();
+            sprintf(s, "%02x", i);
+            boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 0, 16, 15, 31);
+            putfonts8_asc(binfo->vram, binfo->scrnx, 0, 16, COL8_FFFFFF, s);
+        }
     }
 }
